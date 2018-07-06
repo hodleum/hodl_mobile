c206070cb5d3c374449b41047ceda04208e43a7e
vadegsechin <vadim239sch@mail.ru>
2018-07-06 13:42:38 +0300

    fix

--- libs/uix/baseclass/contacts.py
+++ libs/uix/baseclass/contacts.py
@@ -14,18 +14,4 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
 import json
 
-class Contacts (Screen):
-'''
 
-   contacts_list= [{'name': 'Владимир Путенович', 'wallet': 'plotinologevadeg'},
-                   {'name': 'Налогонеплотельщик', 'wallet': 'vadegs'}
-                                                                                 ]
-   for contact in contacts_list:
-       contact_text=(contact['name'])+'\n'+'wallet: '  + (contact['wallet'])
-       print(contact_text)
-       lb = Label(text= contact_text)
-
-       def __init__(self, **kwargs):
-           self.ids.fl.ids.scr.ids.mdl.add_widget(lb)
-
-'''
