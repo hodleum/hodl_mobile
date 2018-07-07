8687134f600e1d8d1c00ae04d48541bb61bf4f90
vadegsechin <vadim239sch@mail.ru>
2018-07-07 16:32:40 +0300

    add good contacts

--- libs/uix/baseclass/contacts.py
+++ libs/uix/baseclass/contacts.py
@@ -20,12 +20,11 @@ from kivy.uix.behaviors import FocusBehavior
 from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 from kivy.metrics import dp
 from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
 
-
-class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
-                                 RecycleBoxLayout):
-    ''' Adds selection and focus behaviour to the view. '''
-
+contacts_list= [{'name': 'Вася', 'wallet':'vasyapupkin'},
+                {'name':'Коля', 'wallet':'nalogoplotelshik'}
+                                                             ]
 
 class SelectableLabel(RecycleDataViewBehavior, Label):
     ''' Add selection support to the Label '''
@@ -33,28 +32,7 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
     selected = BooleanProperty(False)
     selectable = BooleanProperty(True)
 
-    def refresh_view_attrs(self, rv, index, data):
-        ''' Catch and handle the view changes '''
-        self.index = index
-        return super(SelectableLabel, self).refresh_view_attrs(
-            rv, index, data)
-
-    def on_touch_down(self, touch):
-        ''' Add selection on touch down '''
-        if super(SelectableLabel, self).on_touch_down(touch):
-            return True
-        if self.collide_point(*touch.pos) and self.selectable:
-            return self.parent.select_with_touch(self.index, touch)
-
-    def apply_selection(self, rv, index, is_selected):
-        ''' Respond to the selection of items in the view. '''
-        self.selected = is_selected
-        if is_selected:
-            rv.data = []
-            rv.data = [{'text': str(x)} for x in range((index+1)*10)]
-            rv.refresh_from_data()
-
 class Contacts(RecycleView,Screen):
     def __init__(self, **kwargs):
         super(Contacts, self).__init__(**kwargs)
-        self.data = [{'text': str(x)} for x in range(100)]
+        self.data = [{'text': (str(contact['name'] + '\n' + '('+'wallet: '  +str(contact['wallet'])+ ')'))} for contact in contacts_list*50]
