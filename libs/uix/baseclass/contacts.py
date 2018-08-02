633384dcf186bdda4580f44b7fcd934ef16ae785
vadegsechin <vadim239sch@mail.ru>
2018-08-02 23:13:22 +0300

    list of  contacts

--- libs/uix/baseclass/contacts.py
+++ libs/uix/baseclass/contacts.py
@@ -10,25 +10,10 @@
 #
 # LICENSE: MIT
 import os
-import sys
+
 from ast import literal_eval
 
-from kivy.app import App
-from kivy.uix.modalview import ModalView
-from kivy.lang import Builder
-from kivy.core.window import Window
-from kivy.config import ConfigParser
-from kivy.clock import Clock
-from kivy.utils import get_color_from_hex, get_hex_from_color
-from kivy.metrics import dp
-from kivy.properties import ObjectProperty, StringProperty
 
-from main import __version__
-from libs.translation import Translation
-from libs.uix.baseclass.startscreen import StartScreen
-from libs.uix.lists import Lists
-from kivy.uix.textinput import TextInput
-from libs.utils.showplugins import ShowPlugins
 
 from kivy.uix.screenmanager import Screen
 
