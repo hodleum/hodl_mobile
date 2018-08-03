2c1d12fd3756aa2f3376b04b56b249b47e889fb8
vadegsechin <vadim239sch@mail.ru>
2018-08-04 00:08:01 +0300

    fixes

--- libs/uix/baseclass/trade.py
+++ libs/uix/baseclass/trade.py
@@ -9,9 +9,21 @@
 # <kivydevelopment@gmail.com>
 #
 # LICENSE: MIT
-
+import os
+from ast import literal_eval
 from kivy.uix.screenmanager import Screen
 
 
 class Trade (Screen):
-    pass
+    tokens = literal_eval(
+        open(
+            os.path.join('data', 'tokens.txt')).read()
+    )
+    text_amount_of_HDL = str(((tokens['HDL'])['amount'])) + 'HDL'
+
+    sum = 0
+    for token in tokens:
+        sum += tokens[token]['amount'] * tokens[token]['price']
+    sum = round(sum, 7)
+
+    total_amount = 'Total: ' + str(sum) + ' HDL'
