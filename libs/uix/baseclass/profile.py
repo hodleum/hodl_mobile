80c4a1ccef4439cc522cc8c3fd34d66c90cd47af
vadegsechin <vadim239sch@mail.ru>
2018-08-04 23:47:38 +0300

    design

---
+++ libs/uix/baseclass/profile.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# This file created with KivyCreatorProject
+# <https://github.com/HeaTTheatR/KivyCreatorProgect
+#
+# Copyright © 2017 Easy
+#
+# For suggestions and questions:
+# <kivydevelopment@gmail.com>
+#
+# LICENSE: MIT
+
+from kivy.uix.screenmanager import Screen
+
+class Profile (Screen):
+    pass
